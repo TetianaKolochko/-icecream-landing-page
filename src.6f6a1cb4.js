53d94836741dd3adf37b0821a6a651b5f398a83b
MikeTribrat <92452157+MikeTribrat@users.noreply.github.com>
2022-01-15 10:00:40 +0000

    Deploying to gh-pages from @ MikeTribrat/team-project@6d3608e8e69acda6210ac7d0dac35b3cab36c1de 🚀

--- src.6f6a1cb4.js
+++ src.6f6a1cb4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project/src.dc0a443d.js.map
+//# sourceMappingURL=/team-project/src.6f6a1cb4.js.map
